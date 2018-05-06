6e2d243d4aedb6989c2b1debcd004a2d4cdd1047
ivan.lazarte@gmail.com <ilazarte@users.noreply.github.com>
2018-05-06 18:38:08 -0400

    testing CI build

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -9,8 +9,6 @@ public class SpringCoreDevOpsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
-		//this is a whitespace change test
-		// another test...
-		// test me
+		//webhook test
 	}
 }
